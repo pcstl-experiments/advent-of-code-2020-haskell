4579271f8fbd4741ec1eafbc8d6411c4341f1cbd
Pedro Castilho <cstl.px@gmail.com>
2020-12-02 13:29:37 -0300

    Added day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import qualified Day1
+import qualified Day2
 
 main :: IO ()
-main = day1Part1
+main = do
+  putStrLn "-- Day 1 --"
+  putStr "Day 1 Part 1 - Result: "
+  Day1.part1
+  putStr "Day 1 Part 2 - Result: "
+  Day1.part2
+  putStr "\n"
+
+  putStrLn "-- Day 2 --"
+  putStr "Day 2 Part 1 - Result: "
+  Day2.part1
+  putStr "Day 2 Part 2 - Result: "
+  Day2.part2
+  putStr "\n"
